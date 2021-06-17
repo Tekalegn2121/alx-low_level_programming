fe81532593de5190f7ab74ad299a20dd60857648
Tekalegn2121 <tekalegnbareku@gmail.com>
2021-06-17 01:28:38 -0700

    C Hello World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 int main ()
 {
-  write(2, "and that piece of art is useful\" - Dora Korar, 2015-10-19\n", 61);
+  write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 
   return (1);
 }
