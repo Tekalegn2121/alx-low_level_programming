523cc21075196143afd43ba08d3aeb4c28c825e4
Tekalegn2121 <tekalegnbareku@gmail.com>
2021-06-17 01:35:28 -0700

    C Hello World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
-int main ()
-{
-  write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+/**
+ *main - Enytry point
+ *
+ *Return: Always 0 (success)
+ */
 
+int main(void)
+{
+  write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
   return (1);
 }
