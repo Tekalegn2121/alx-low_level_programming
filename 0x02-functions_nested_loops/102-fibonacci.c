111d7e3a08965a04ce46a02499b5cb16b48fe2d0
Tekalegn2121 <tekalegnbareku@gmail.com>
2021-06-22 07:04:38 -0700

    Function and nested  loops

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,12 +2,26 @@
 #include <stdlib.h>
 #include "holberton.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
+int counter;
+int countto = 50;
+long a = 1;
+long b = 2;
 
+for (counter = 1; counter <= (countto / 2); counter++)
+{
+printf("%li %li ", a, b);
+a += b;
+b += a;
+}
+if (countto % 2 == 1)
+printf("%li", a);
+
+printf("\n");
 return (0);
 }
