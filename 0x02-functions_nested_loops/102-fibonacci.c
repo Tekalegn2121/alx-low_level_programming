0acea34c9a5f5d86aabc6c6f1f4666e0f4bbf51d
Tekalegn2121 <tekalegnbareku@gmail.com>
2021-06-22 08:55:43 -0700

    Function and nested  Loop

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "holberton.h"
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
+* main - print first 50 Fibonacci numbers
+* Return: 0
 */
 int main(void)
 {
@@ -12,7 +9,6 @@ int counter;
 int countto = 50;
 long a = 1;
 long b = 2;
-
 for (counter = 1; counter <= (countto / 2); counter++)
 {
 printf("%li %li ", a, b);
@@ -20,8 +16,7 @@ a += b;
 b += a;
 }
 if (countto % 2 == 1)
-  printf("%li", a);
-
+printf("%li", a);
 printf("\n");
 return (0);
 }
