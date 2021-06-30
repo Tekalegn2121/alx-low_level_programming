43eeb99ff9b4e9d668908ec0fe0614e657748e4f
Tekalegn2121 <tekalegnbareku@gmail.com>
2021-06-30 06:17:38 -0700

    Pointers, arrays and Strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,56 @@
+#include "holberton.h"
+/**
+ * _strncpy - copy a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+  int j;
+
+  j = 0;
+  while (j < n && src[j] != '\0')
+    {
+      dest[j] = src[j];
+      j++;
+    }
+  while (j < n)
+    {
+      dest[j] = '\0';
+      j++;
+    }
+
+  return (dest);
+}
+#include "holberton.h"
+/**
+ * _strncpy - copy a string
+ * @dest: input value
+#include "holberton.h"
+/**
+* _strncpy - copy a string
+* @src: input value
+* @n: input value
+*
+* Return: dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+int j;
+
+j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[j] = src[j];
+j++;
+}
+while (j < n)
+{
+dest[j] = '\0';
+j++;
+}
+return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is arrays strings and pointers
