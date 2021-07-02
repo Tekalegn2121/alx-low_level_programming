902ccbc54c4292cd7efedc96009758cad661bbbc
Tekalegn2121 <tekalegnbareku@gmail.com>
2021-07-02 01:37:51 -0700

    Pointer arrays and string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,29 +1,33 @@
 #include "holberton.h"
+
 /**
- * _strncpy - copy a string
- * @dest: input value
-#include "holberton.h"
-/**
-* _strncpy - copy a string
-* @src: input value
-* @n: input value
+* _strncpy - Copy a string
+* @dest: The destination value
+* @src: The source value
+* @n: The copy limit
 *
-* Return: dest
+* Return: char value
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-int j;
+int a = 0, b = 0;
 
-j = 0;
-while (j < n && src[j] != '\0')
+while (src[b])
 {
-dest[j] = src[j];
-j++;
+b++;
 }
-while (j < n)
+
+while (a < n && src[a])
 {
-dest[j] = '\0';
-j++;
+dest[a] = src[a];
+a++;
 }
+
+while (a < n)
+{
+dest[a] = '\0';
+a++;
+}
+
 return (dest);
 }
