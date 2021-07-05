b4dd66356a9872f6ad1c761630c42d2a5481c265
Tekalegn2121 <tekalegnbareku@gmail.com>
2021-07-05 19:03:01 +0300

    Pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code inside the body
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *t0 = "Teka Bareku";
+    char *t1 = "Fasika molla";
+
+    printf("%st, %t\n", t0, t1);
+    set_string(&t1, ts0);
+    printf("%t, %t\n", s0, t1);
+    return (0);
+}
