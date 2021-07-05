bcce56a377a00703d6297e8007a61eab5b88bf76
Tekalegn2121 <tekalegnbareku@gmail.com>
2021-07-05 18:51:37 +0300

    Pointers, arrays and strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,19 +1,29 @@
 #include "holberton.h"
-#include <stddef.h>
+
 /**
-* _strchr - locates a character in a string
-* @s: the string
-* @c: the character
-*
-* Return: a pointer to a character aka the character found
-*/
+ * _strchr - locate character in string
+ * @s: source string
+ * @c: character to find
+ *
+ * Return: the string from character found
+ */
 char *_strchr(char *s, char c)
 {
-int i;
-for (i = 0; s[i] != '\0' ; i++)
-{
-if (s[i] == c)
-return (&s[i]);
-}
-return (0);
+	int a = 0, b;
+
+	while (s[a])
+	{
+		a++;
+	}
+
+	for (b = 0; b <= a; b++)
+	{
+		if (c == s[b])
+		{
+			s += b;
+			return (s);
+		}
+	}
+
+	return ('\0');
 }
